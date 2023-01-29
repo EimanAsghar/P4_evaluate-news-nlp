19958ef2b70ab456d154e2d6059af3023211f326
Eiman Asghar <65137832+EimanAsghar@users.noreply.github.com>
2023-01-29 22:41:45 +0300

    change

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,13 +14,22 @@ module.exports = {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
-    ]
+        }),
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+        new WorkboxPlugin.GenerateSW()
+    ],
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    }
 }
