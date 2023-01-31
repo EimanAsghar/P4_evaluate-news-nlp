4c9243d308e096591bc81f9bfd19e15df248cdd0
Eiman Asghar <65137832+EimanAsghar@users.noreply.github.com>
2023-02-01 00:16:48 +0300

    update

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -19,7 +19,7 @@ const handleSubmit = async() => {
     let url  = document.getElementById('url').value
     if(Client.checkURL(url)){
         post('http://localhost:8081/sendData', {url: url}).then(data =>{
-            document.getElementById('polarity').innerHTML = 'Polarity: '+polarityChecker(data.score_tag);
+            document.getElementById('polarity').innerHTML =  `Polarity: ${data.score_tag}`;
             document.getElementById("agreement").innerHTML = `Agreement: ${data.agreement}`;
             document.getElementById("subjectivity").innerHTML = `Subjectivity: ${data.subjectivity}`;
             document.getElementById("confidence").innerHTML = `Confidence: ${data.confidence}`;
@@ -29,30 +29,6 @@ const handleSubmit = async() => {
     } else{
         alert('Enter another URL!')
     }
-}
-
-const polarityChecker = (score) => {
-    let display;
-    switch (score){
-        case 'P+':
-            display = 'strong positive';
-            break;
-        case 'P':
-            display = 'positive';
-            break;
-        case 'NEW':
-            display = 'neutral';
-            break;
-        case 'N':
-            display = 'negative';
-            break;
-        case 'N+':
-            display = 'strong negative';
-            break;
-        case 'NONE':
-            display = 'no sentiment';
-    }
-    return display.toUpperCase();
 }
 
 export { handleSubmit }
